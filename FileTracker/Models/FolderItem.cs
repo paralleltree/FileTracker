f80fc3916422bf272ba16475c3245c3778298a6b
paralleltree <paralleltree@outlook.com>
2014-12-11 17:44:16 +0900

    FolderItemクラスを追加

---
+++ FileTracker/Models/FolderItem.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTracker.Models
+{
+    class FolderItem : IDisposable
+    {
+        private FileSystemWatcher Watcher { get; set; }
+
+        public string Path
+        {
+            get { return Watcher.Path; }
+            set { Watcher.Path = value; }
+        }
+
+        public bool IsWatching
+        {
+            get { return Watcher.EnableRaisingEvents; }
+            set { Watcher.EnableRaisingEvents = value; }
+        }
+
+
+        public FolderItem(string path)
+        {
+            Watcher = new FileSystemWatcher(path)
+            {
+                EnableRaisingEvents = true
+            };
+            Watcher.Created += OnCreated;
+            Watcher.Changed += OnChanged;
+            Watcher.Deleted += OnDeleted;
+            Watcher.Renamed += OnRenamed;
+        }
+
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+
+        }
+
+
+        public void Dispose()
+        {
+            if (this.Watcher != null)
+            {
+                Watcher.Created -= OnCreated;
+                Watcher.Changed -= OnChanged;
+                Watcher.Deleted -= OnDeleted;
+                Watcher.Renamed -= OnRenamed;
+                Watcher.Dispose();
+            }
+        }
+    }
+}
