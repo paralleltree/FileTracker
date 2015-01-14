d6b2dee4c366740b9586b4000c6de352e5cd002e
paralleltree <paralleltree@outlook.com>
2015-01-14 19:27:38 +0900

    レポーターのModelクラスを作成

--- FileTracker.Reporter/Models/Model.cs
+++ FileTracker.Reporter/Models/Model.cs
@@ -9,8 +9,44 @@ namespace FileTracker.Reporter.Models
 {
     public class Model : NotificationObject
     {
+        private Model()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                IsInfoGiven = true;
+                InfoPath = args[1];
+                ExceptionInfo = ExceptionInfo.ReadInfo(args[1]);
+            }
+        }
+
+        static Model _instance;
+        public static Model Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Model();
+                return _instance;
+            }
+        }
+
         /*
          * NotificationObjectはプロパティ変更通知の仕組みを実装したオブジェクトです。
          */
+
+        /// <summary>
+        /// 例外情報が渡されたかどうかを格納します。
+        /// </summary>
+        public bool IsInfoGiven { get; private set; }
+
+        /// <summary>
+        /// 渡された例外情報のパスを格納します。
+        /// </summary>
+        public string InfoPath { get; private set; }
+
+        /// <summary>
+        /// 渡された例外情報を格納します。
+        /// </summary>
+        public ExceptionInfo ExceptionInfo { get; private set; }
     }
 }
